8bdae5c32cc7884fe4d9eac80fdc082f13a8d595
puriamoros <puri.amoros@gmail.com>
2016-05-17 12:46:21 +0200

    refactorización

---
+++ MyStyleApp/MyStyleApp/MyStyleApp/Services/Backend/BackendServiceBase.cs
@@ -0,0 +1,20 @@
+﻿namespace MyStyleApp.Services.Backend
+{
+    public abstract class BackendServiceBase
+    {
+        private HttpService _httpService;
+
+        public BackendServiceBase(HttpService httpService)
+        {
+            this._httpService = httpService;
+        }
+
+        public HttpService HttpService
+        {
+            get
+            {
+                return this._httpService;
+            }
+        }
+    }
+}

eb66b4f618e3e30469ffe371179348a47e3559a0
puriamoros <puri.amoros@gmail.com>
2016-06-30 14:42:01 +0200

    BackendStatusCodeEnum

---
+++ MyStyleApp/MyStyleApp/MyStyleApp/Enums/BackendStatusCodeEnum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStyleApp.Enums
+{
+    public enum BackendStatusCodeEnum
+    {
+        StateInvalidUrl = 1,
+        StateDbError,
+        StateInvalidMethod,
+        StateNotAuthorized,
+        StateInvalidOperation,
+        StateInvalidData,
+        StateDuplicatedKeyError
+    }
+}

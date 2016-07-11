fc1d1927e8c3399e4d65af32896cb23fe1c48042
puriamoros <puri.amoros@gmail.com>
2016-07-11 12:55:12 +0200

    Pantalla de splash en android

--- MyStyleApp/MyStyleApp/MyStyleApp.Droid/MainActivity.cs
+++ MyStyleApp/MyStyleApp/MyStyleApp.Droid/MainActivity.cs
@@ -9,15 +9,22 @@ using Android.OS;
 
 namespace MyStyleApp.Droid
 {
-    [Activity(Label = "MyStyleApp", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Theme = "@style/Theme.Splash", Label = "MyStyleApp", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
     {
         protected override void OnCreate(Bundle bundle)
         {
+            // Revert theme and window features from the splash.
+            base.Window.RequestFeature(WindowFeatures.ActionBar);
+            base.SetTheme(global::Android.Resource.Style.ThemeDeviceDefault);
             base.OnCreate(bundle);
+
+            base.ActionBar.Hide();
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App());
+
+            base.ActionBar.Show();
         }
     }
 }

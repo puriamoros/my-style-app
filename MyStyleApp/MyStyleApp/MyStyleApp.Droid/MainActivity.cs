1f65a3261b8a0e0abd55f8f82535e98e10dcd4b5
puriamoros <puri.amoros@gmail.com>
2016-08-16 19:34:30 +0200

    Ajustes

--- MyStyleApp/MyStyleApp/MyStyleApp.Droid/MainActivity.cs
+++ MyStyleApp/MyStyleApp/MyStyleApp.Droid/MainActivity.cs
@@ -17,8 +17,8 @@ namespace MyStyleApp.Droid
         protected override void OnCreate(Bundle bundle)
         {
             // Revert theme and window features from the splash.
-            base.Window.RequestFeature(WindowFeatures.ActionBar);
-            base.SetTheme(global::Android.Resource.Style.ThemeDeviceDefault);
+            //base.SetTheme(global::Android.Resource.Style.ThemeDeviceDefault);
+            base.SetTheme(Resource.Style.Theme_Activity);
             base.OnCreate(bundle);
 
             base.ActionBar.Hide();
@@ -29,26 +29,7 @@ namespace MyStyleApp.Droid
 
             // Hide navigation bar icon
             base.ActionBar.SetIcon(Android.Resource.Color.Transparent);
-
-            //// Center navigation bar title: DO NOT WORK WELL IN ALL DEVICES
-            //int titleId = 0;
-            //if (Build.VERSION.SdkInt >= BuildVersionCodes.Honeycomb)
-            //{
-            //    titleId = Resources.GetIdentifier("action_bar_title", "id", "android");
-            //}
-            //else
-            //{
-            //    titleId = Resource.Id.action_bar_title;
-            //}
-            //var titleTextView = FindViewById<TextView>(titleId);
-            //var layoutParams = (LinearLayout.LayoutParams)titleTextView.LayoutParameters;
-            //layoutParams.Gravity = GravityFlags.CenterHorizontal;
-            //layoutParams.Width = Resources.DisplayMetrics.WidthPixels;
-            //titleTextView.LayoutParameters = layoutParams;
-            //titleTextView.Gravity = GravityFlags.Center;
-            //// Added padding because it is slightly off centered
-            ////titleTextView.SetPadding(-150, 0, 0, 0);
-
+            
             base.ActionBar.Show();
         }
     }
--- MyStyleApp/MyStyleApp/MyStyleApp.Droid/Resources/Resource.Designer.cs
+++ MyStyleApp/MyStyleApp/MyStyleApp.Droid/Resources/Resource.Designer.cs
@@ -1160,35 +1160,38 @@ namespace MyStyleApp.Droid
 		public partial class Color
 		{
 			
+			// aapt resource value: 0x7f08005a
+			public const int LongPressedHighlight = 2131230810;
+			
 			// aapt resource value: 0x7f080059
 			public const int SplashBackground = 2131230809;
 			
-			// aapt resource value: 0x7f08005a
-			public const int abc_background_cache_hint_selector_material_dark = 2131230810;
-			
 			// aapt resource value: 0x7f08005b
-			public const int abc_background_cache_hint_selector_material_light = 2131230811;
+			public const int abc_background_cache_hint_selector_material_dark = 2131230811;
 			
 			// aapt resource value: 0x7f08005c
-			public const int abc_color_highlight_material = 2131230812;
+			public const int abc_background_cache_hint_selector_material_light = 2131230812;
+			
+			// aapt resource value: 0x7f08005d
+			public const int abc_color_highlight_material = 2131230813;
 			
 			// aapt resource value: 0x7f080015
 			public const int abc_input_method_navigation_guard = 2131230741;
 			
-			// aapt resource value: 0x7f08005d
-			public const int abc_primary_text_disable_only_material_dark = 2131230813;
-			
 			// aapt resource value: 0x7f08005e
-			public const int abc_primary_text_disable_only_material_light = 2131230814;
+			public const int abc_primary_text_disable_only_material_dark = 2131230814;
 			
 			// aapt resource value: 0x7f08005f
-			public const int abc_primary_text_material_dark = 2131230815;
+			public const int abc_primary_text_disable_only_material_light = 2131230815;
 			
 			// aapt resource value: 0x7f080060
-			public const int abc_primary_text_material_light = 2131230816;
+			public const int abc_primary_text_material_dark = 2131230816;
 			
 			// aapt resource value: 0x7f080061
-			public const int abc_search_url_text = 2131230817;
+			public const int abc_primary_text_material_light = 2131230817;
+			
+			// aapt resource value: 0x7f080062
+			public const int abc_search_url_text = 2131230818;
 			
 			// aapt resource value: 0x7f080016
 			public const int abc_search_url_text_normal = 2131230742;
@@ -1199,11 +1202,11 @@ namespace MyStyleApp.Droid
 			// aapt resource value: 0x7f080018
 			public const int abc_search_url_text_selected = 2131230744;
 			
-			// aapt resource value: 0x7f080062
-			public const int abc_secondary_text_material_dark = 2131230818;
-			
 			// aapt resource value: 0x7f080063
-			public const int abc_secondary_text_material_light = 2131230819;
+			public const int abc_secondary_text_material_dark = 2131230819;
+			
+			// aapt resource value: 0x7f080064
+			public const int abc_secondary_text_material_light = 2131230820;
 			
 			// aapt resource value: 0x7f080019
 			public const int accent_material_dark = 2131230745;
@@ -1262,8 +1265,8 @@ namespace MyStyleApp.Droid
 			// aapt resource value: 0x7f080008
 			public const int common_action_bar_splitter = 2131230728;
 			
-			// aapt resource value: 0x7f080064
-			public const int common_google_signin_btn_text_dark = 2131230820;
+			// aapt resource value: 0x7f080065
+			public const int common_google_signin_btn_text_dark = 2131230821;
 			
 			// aapt resource value: 0x7f080009
 			public const int common_google_signin_btn_text_dark_default = 2131230729;
@@ -1277,8 +1280,8 @@ namespace MyStyleApp.Droid
 			// aapt resource value: 0x7f08000a
 			public const int common_google_signin_btn_text_dark_pressed = 2131230730;
 			
-			// aapt resource value: 0x7f080065
-			public const int common_google_signin_btn_text_light = 2131230821;
+			// aapt resource value: 0x7f080066
+			public const int common_google_signin_btn_text_light = 2131230822;
 			
 			// aapt resource value: 0x7f08000d
 			public const int common_google_signin_btn_text_light_default = 2131230733;
@@ -1292,8 +1295,8 @@ namespace MyStyleApp.Droid
 			// aapt resource value: 0x7f08000e
 			public const int common_google_signin_btn_text_light_pressed = 2131230734;
 			
-			// aapt resource value: 0x7f080066
-			public const int common_plus_signin_btn_text_dark = 2131230822;
+			// aapt resource value: 0x7f080067
+			public const int common_plus_signin_btn_text_dark = 2131230823;
 			
 			// aapt resource value: 0x7f080000
 			public const int common_plus_signin_btn_text_dark_default = 2131230720;
@@ -1307,8 +1310,8 @@ namespace MyStyleApp.Droid
 			// aapt resource value: 0x7f080001
 			public const int common_plus_signin_btn_text_dark_pressed = 2131230721;
 			
-			// aapt resource value: 0x7f080067
-			public const int common_plus_signin_btn_text_light = 2131230823;
+			// aapt resource value: 0x7f080068
+			public const int common_plus_signin_btn_text_light = 2131230824;
 			
 			// aapt resource value: 0x7f080004
 			public const int common_plus_signin_btn_text_light_default = 2131230724;
@@ -1466,11 +1469,11 @@ namespace MyStyleApp.Droid
 			// aapt resource value: 0x7f08004c
 			public const int switch_thumb_disabled_material_light = 2131230796;
 			
-			// aapt resource value: 0x7f080068
-			public const int switch_thumb_material_dark = 2131230824;
-			
 			// aapt resource value: 0x7f080069
-			public const int switch_thumb_material_light = 2131230825;
+			public const int switch_thumb_material_dark = 2131230825;
+			
+			// aapt resource value: 0x7f08006a
+			public const int switch_thumb_material_light = 2131230826;
 			
 			// aapt resource value: 0x7f08004d
 			public const int switch_thumb_normal_material_dark = 2131230797;
@@ -4212,6 +4215,9 @@ namespace MyStyleApp.Droid
 			
 			// aapt resource value: 0x7f0b00ff
 			public const int TextAppearance_Widget_AppCompat_Toolbar_Title = 2131427583;
+			
+			// aapt resource value: 0x7f0b0173
+			public const int Theme_Activity = 2131427699;
 			
 			// aapt resource value: 0x7f0b0100
 			public const int Theme_AppCompat = 2131427584;
